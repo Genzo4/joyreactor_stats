fd17f53b021f98ad093a3e9b4fe7218864ff788d
Genzo4 <genzo@bk.ru>
2023-08-07 16:20:34 +1000

    time format

--- joyreactor_stats/__init__.py
+++ joyreactor_stats/__init__.py
@@ -65,7 +65,7 @@ class JoyreactorStats:
         self.print_msg("Сохраняем отчёт в excel ...")
         save_date = datetime.now()
 
-        xls_file = os.path.join(self.WORK_FOLDER, f'joyreactor_{self.account}_{save_date.strftime("%d.%m.%Y %H:%M")}.xlsx')
+        xls_file = os.path.join(self.WORK_FOLDER, f'joyreactor_{self.account}_{save_date.strftime("%d.%m.%Y_%H:%M")}.xlsx')
 
         self.save_report(xls_file)
 
